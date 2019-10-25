2eb9145143a62fe7c527c039371b37f403daa55d
burbage stacey <rstacey06@gmail.com>
2019-10-25 18:32:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,21 +14,17 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-roll_call_dwarves(dwarves)
-
 planeteer_calls = %w[earth wind fire water heart]
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
-summon_captain_planet(planeteer_calls)
-
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
 
-long_planeteer_calls(planeteer_calls)
+
 
 potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
 
